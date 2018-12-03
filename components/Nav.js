0326eaa4888f3a249f399252f51f9529fe1f0376
Hendrik Wendt <h.wendt@gmx.com>
2018-12-03 10:27:55 -0500

    intial setup

--- components/Nav.js
+++ components/Nav.js
@@ -4,7 +4,7 @@ import NavStyles from './styles/NavStyles';
 const Nav = () => (
   <NavStyles>
     <Link href="/items">
-      <a>Itmes</a>
+      <a>Items</a>
     </Link>
     <Link href="/sell">
       <a>Sell</a>
