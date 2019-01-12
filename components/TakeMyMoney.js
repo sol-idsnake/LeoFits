819e58b6f9d98d755240464ff1ee606ff1e93e14
unknown <h.wendt@gmx.com>
2019-01-12 13:09:29 -0500

    updated

--- components/TakeMyMoney.js
+++ components/TakeMyMoney.js
@@ -9,14 +9,36 @@ import calcTotalPrice from '../lib/calcTotalPrice';
 import Error from './ErrorMessage';
 import User, { CURRENT_USER_QUERY } from './User';
 
+const CREATE_ORDER_MUTATION = gql`
+  mutation createOrder($token: String!) {
+    createOrder(token: $token) {
+      id
+      charge
+      total
+      items {
+        id
+        title
+      }
+    }
+  }
+`;
+
 function totalItems(cart) {
   return cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0);
 }
 
 export class TakeMyMoney extends Component {
-  onToken(res) {
+  onToken(res, createOrder) {
     // https://stripe.com/docs/testing#cards
-    console.log(res);
+    console.log(this.res);
+    // manually call the mutation once we have the stripe token
+    createOrder({
+      variables: {
+        token: res.id,
+      },
+    }).catch(err => {
+      alert(err.message);
+    });
   }
 
   render() {
@@ -24,20 +46,27 @@ export class TakeMyMoney extends Component {
     return (
       <User>
         {({ data: { me } }) => (
-          <StripeCheckout
-            // https://github.com/azmenak/react-stripe-checkout#send-all-the-props
-            // always have to send Cents to stripe
-            amount={calcTotalPrice(me.cart)}
-            image={me.cart[0].item && me.cart[0].item.image}
-            name="LEO Fits"
-            description={`Order of ${totalItems(me.cart)} items!`}
-            stripeKey="pk_test_8QLFclj8SDmalnV6Jl1s9XG0"
-            currency="USD"
-            email={me.email}
-            token={res => this.onToken(res)}
+          <Mutation
+            mutation={CREATE_ORDER_MUTATION}
+            refetchQueries={[{ query: CURRENT_USER_QUERY }]}
           >
-            {children}
-          </StripeCheckout>
+            {createOrder => (
+              <StripeCheckout
+                // https://github.com/azmenak/react-stripe-checkout#send-all-the-props
+                // always have to send Cents to stripe
+                amount={calcTotalPrice(me.cart)}
+                image={me.cart[0].item && me.cart[0].item.image}
+                name="LEO Fits"
+                description={`Order of ${totalItems(me.cart)} items!`}
+                stripeKey="pk_test_8QLFclj8SDmalnV6Jl1s9XG0"
+                currency="USD"
+                email={me.email}
+                token={res => this.onToken(res, createOrder)}
+              >
+                {children}
+              </StripeCheckout>
+            )}
+          </Mutation>
         )}
       </User>
     );
