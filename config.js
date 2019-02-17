3aa30b797777b4897afb5538d66052b7c3247691
Hendrik Wendt <h.wendt@gmx.com>
2019-02-17 18:59:21 -0500

    backend url

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export const endpoint = `http://localhost:4444`;
-export const prodEndpoint = `https://leofitsbackend-xjffotxcwe.now.sh/`;
+export const prodEndpoint = `https://leofitsbackend-qlrjcwubmu.now.sh/`;
 export const perPage = 4;
